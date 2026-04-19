8ec305e2de59251ead4f66c45a839a63b3598bdf
Xonia88 <118845211+Xonia88@users.noreply.github.com>
2026-04-19 22:39:29 +0300

    Task 2 - Vectori: filtrare, mutare si concatenare pentru Rezervare

--- Rezervare.c
+++ Rezervare.c
@@ -59,21 +59,113 @@ void dezalocare(Rezervare* r) {
 	free(r->preturi);
 }
 
+Rezervare copiazaRezervare(Rezervare r) {
+	return creareRezervare(r.id, r.numeClient, r.nrLocuri, r.preturi);
+}
+
+void afisareVector(Rezervare* v, int n) {
+	for (int i = 0; i < n; i++) {
+		afisareRezervare(v[i]);
+	}
+}
+
+Rezervare* filtreazaRezervari(Rezervare* v, int n, float prag, int* dimNou) {
+	*dimNou = 0;
+	for (int i = 0; i < n; i++) {
+		if (calculeazaTotal(v[i]) > prag) {
+			(*dimNou)++;
+		}
+	}
+
+	Rezervare* vNou = (Rezervare*)malloc(sizeof(Rezervare) * (*dimNou));
+
+	int k = 0;
+	for (int i = 0; i < n; i++) {
+		if (calculeazaTotal(v[i]) > prag) {
+			vNou[k++] = copiazaRezervare(v[i]);
+		}
+	}
+
+	return vNou;
+}
+
+Rezervare* mutaRezervari(Rezervare* v, int n, float prag, int* dimNou) {
+	*dimNou = 0;
+	for (int i = 0; i < n; i++) {
+		if (calculeazaTotal(v[i]) < prag) {
+			(*dimNou)++;
+		}
+	}
+
+	Rezervare* vNou = (Rezervare*)malloc(sizeof(Rezervare) * (*dimNou));
+
+	int k = 0;
+	for (int i = 0; i < n; i++) {
+		if (calculeazaTotal(v[i]) < prag) {
+			vNou[k++] = copiazaRezervare(v[i]);
+		}
+	}
+
+	return vNou;
+}
+
+Rezervare* concateneazaVectori(Rezervare* v1, int n1, Rezervare* v2, int n2) {
+	Rezervare* vNou = (Rezervare*)malloc(sizeof(Rezervare) * (n1 + n2));
+
+	for (int i = 0; i < n1; i++) {
+		vNou[i] = copiazaRezervare(v1[i]);
+	}
+
+	for (int i = 0; i < n2; i++) {
+		vNou[n1 + i] = copiazaRezervare(v2[i]);
+	}
+
+	return vNou;
+}
+
+void dezalocareVector(Rezervare** v, int* n) {
+	for (int i = 0; i < *n; i++) {
+		dezalocare(&((*v)[i]));
+	}
+	free(*v);
+	*v = NULL;
+	*n = 0;
+}
+
 int main() {
-	float preturi[] = { 100, 150, 200 };
+	int n = 5;
+	Rezervare* v = (Rezervare*)malloc(sizeof(Rezervare) * n);
 
-	Rezervare r = creareRezervare(1, "Popescu Ion", 3, preturi);
+	float p1[] = { 100, 100 };
+	float p2[] = { 200, 200 };
+	float p3[] = { 50, 50 };
+	float p4[] = { 300 };
+	float p5[] = { 150, 150 };
 
-	afisareRezervare(r);
+	v[0] = creareRezervare(1, "Ana", 2, p1);
+	v[1] = creareRezervare(2, "Ion", 2, p2);
+	v[2] = creareRezervare(3, "Maria", 2, p3);
+	v[3] = creareRezervare(4, "Paul", 1, p4);
+	v[4] = creareRezervare(5, "Elena", 2, p5);
 
-	printf("Total rezervare: %.2f\n", calculeazaTotal(r));
+	afisareVector(v, n);
 
-	modificaNumeClient(&r, "Ionescu Ana");
+	int dimFiltrat = 0;
+	Rezervare* vFiltrat = filtreazaRezervari(v, n, 250, &dimFiltrat);
+	afisareVector(vFiltrat, dimFiltrat);
 
-	printf("\nDupa modificare:\n");
-	afisareRezervare(r);
+	int dimMutat = 0;
+	Rezervare* vMutat = mutaRezervari(v, n, 250, &dimMutat);
+	afisareVector(vMutat, dimMutat);
 
-	dezalocare(&r);
+	int dimConcat = dimFiltrat + dimMutat;
+	Rezervare* vConcat = concateneazaVectori(vFiltrat, dimFiltrat, vMutat, dimMutat);
+	afisareVector(vConcat, dimConcat);
+
+	dezalocareVector(&v, &n);
+	dezalocareVector(&vFiltrat, &dimFiltrat);
+	dezalocareVector(&vMutat, &dimMutat);
+	dezalocareVector(&vConcat, &dimConcat);
 
 	return 0;
 }
