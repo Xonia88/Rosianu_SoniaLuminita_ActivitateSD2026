e391e4b220d1decd091576e3d5584de2a7e9757a
Xonia88 <118845211+Xonia88@users.noreply.github.com>
2026-04-21 21:11:33 +0300

    HashTable angajati

---
+++ hashtable_angajati.c
@@ -0,0 +1,141 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<malloc.h>
+#include<string.h>
+
+typedef struct Angajat Angajat;
+typedef struct Nod Nod;
+typedef struct HashTable HashTable;
+
+struct Angajat {
+	char* nume;
+	int id;
+	float salariu;
+};
+
+struct Nod {
+	Angajat info;
+	Nod* next;
+};
+
+struct HashTable {
+	int dimensiune;
+	Nod** vector;
+};
+
+Angajat initAngajat(const char* nume, int id, float salariu) {
+	Angajat a;
+	a.id = id;
+	a.salariu = salariu;
+
+	a.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
+	strcpy(a.nume, nume);
+
+	return a;
+}
+
+void afisareAngajat(Angajat a) {
+	printf("\nAngajatul %s (ID: %d) are salariul %.2f",
+		a.nume, a.id, a.salariu);
+}
+
+void afisareLista(Nod* cap) {
+	while (cap != NULL) {
+		afisareAngajat(cap->info);
+		cap = cap->next;
+	}
+}
+
+void inserareLaSfarsit(Nod** cap, Angajat a) {
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->info = a;
+	nou->next = NULL;
+
+	if (*cap == NULL) {
+		*cap = nou;
+	}
+	else {
+		Nod* temp = *cap;
+		while (temp->next != NULL) {
+			temp = temp->next;
+		}
+		temp->next = nou;
+	}
+}
+
+HashTable initHashTable(int dim) {
+	HashTable ht;
+	ht.dimensiune = dim;
+	ht.vector = (Nod**)malloc(sizeof(Nod*) * dim);
+
+	for (int i = 0; i < dim; i++) {
+		ht.vector[i] = NULL;
+	}
+
+	return ht;
+}
+
+int hash(int dim, int id) {
+	return id % dim;
+}
+
+void inserareHashTable(HashTable ht, Angajat a) {
+	if (ht.dimensiune > 0) {
+		int poz = hash(ht.dimensiune, a.id);
+		if (poz >= 0 && poz < ht.dimensiune) {
+			inserareLaSfarsit(&(ht.vector[poz]), a);
+		}
+	}
+}
+
+void afisareHashTable(HashTable ht) {
+	for (int i = 0; i < ht.dimensiune; i++) {
+		printf("\nPozitie %d:", i);
+		afisareLista(ht.vector[i]);
+	}
+}
+
+float calculSalariiPrimaLista(HashTable ht) {
+	float suma = 0;
+	Nod* cap = ht.vector[0];
+
+	while (cap != NULL) {
+		suma += cap->info.salariu;
+		cap = cap->next;
+	}
+
+	return suma;
+}
+
+void dezalocareLista(Nod** cap) {
+	while (*cap != NULL) {
+		free((*cap)->info.nume);
+		Nod* temp = *cap;
+		*cap = (*cap)->next;
+		free(temp);
+	}
+}
+
+void dezalocareHashTable(HashTable ht) {
+	for (int i = 0; i < ht.dimensiune; i++) {
+		dezalocareLista(&(ht.vector[i]));
+	}
+	free(ht.vector);
+}
+
+int main() {
+	HashTable ht = initHashTable(4);
+
+	inserareHashTable(ht, initAngajat("Ana", 10, 3500));
+	inserareHashTable(ht, initAngajat("Ion", 15, 4200));
+	inserareHashTable(ht, initAngajat("Maria", 22, 3900));
+
+	afisareHashTable(ht);
+
+	printf("\n\nSuma salariilor din prima lista: %.2f",
+		calculSalariiPrimaLista(ht));
+
+	dezalocareHashTable(ht);
+
+	return 0;
+}
--- student_stiva_coada.c
+++ student_stiva_coada.c
@@ -1,111 +1,111 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<malloc.h>
-#include<string.h>
-
-typedef struct Student Student;
-typedef struct Nod Nod;
-
-struct Student {
-	char* nume;
-	int varsta;
-	float medie;
-};
-
-struct Nod {
-	Student info;
-	Nod* next;
-};
-
-Student initStudent(const char* nume, int varsta, float medie) {
-	Student s;
-	s.varsta = varsta;
-	s.medie = medie;
-
-	if (nume != NULL) {
-		s.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
-		strcpy(s.nume, nume);
-
-	}
-	else {
-		s.nume = NULL;
-	}
-	return s;
-}
-
-void afisareStudent(Student s) {
-	printf("\nStudentul %s are varsta %d si media %.2f",
-		s.nume, s.varsta, s.medie);
-}
-void push(Nod** varf, Student s) {
-	Nod* nou = (Nod*)malloc(sizeof(Nod));
-	nou->info = s;
-	nou->next = *varf;
-	*varf = nou;
-}
-
-Student pop(Nod** varf) {
-	if (*varf == NULL) {
-		return initStudent(NULL, 0, 0);
-
-	}
-	Student s = (*varf)->info;
-	Nod* temp = *varf;
-	*varf = (*varf)->next;
-	free(temp);
-
-	return s;
-}
-
-void put(Nod** coada, Student s) {
-	Nod* nou = (Nod*)malloc(sizeof(Nod));
-	nou->info = s;
-	nou->next = NULL;
-
-	if (*coada == NULL) {
-		*coada = nou;
-	}
-	else {
-		Nod* temp = *coada;
-		while (temp->next != NULL) {
-			temp = temp->next;
-		}
-		temp->next = nou;
-	}
-
-}
-
-int main() {
-
-	Nod* stiva = NULL;
-
-	push(&stiva, initStudent("Ana", 21, 7.5));
-	push(&stiva, initStudent("Andreea", 19, 7.5));
-	push(&stiva, initStudent("Vica", 22, 8));
-
-
-	printf("\nTraversare stiva: ");
-
-	while (stiva != NULL) {
-		Student s = pop(&stiva);
-		afisareStudent(s);
-		free(s.nume);
-	}
-
-	Nod* coada = NULL;
-
-	put(&coada, initStudent("Ana", 21, 7.5));
-	put(&coada, initStudent("Andreea", 19, 7.5));
-	put(&coada, initStudent("Vica", 22, 8));
-
-
-	printf("\nTraversare coada: ");
-
-	while (coada != NULL) {
-		Student s = pop(&coada);
-		afisareStudent(s);
-		free(s.nume);
-	}
-
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<malloc.h>
+//#include<string.h>
+//
+//typedef struct Student Student;
+//typedef struct Nod Nod;
+//
+//struct Student {
+//	char* nume;
+//	int varsta;
+//	float medie;
+//};
+//
+//struct Nod {
+//	Student info;
+//	Nod* next;
+//};
+//
+//Student initStudent(const char* nume, int varsta, float medie) {
+//	Student s;
+//	s.varsta = varsta;
+//	s.medie = medie;
+//
+//	if (nume != NULL) {
+//		s.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
+//		strcpy(s.nume, nume);
+//
+//	}
+//	else {
+//		s.nume = NULL;
+//	}
+//	return s;
+//}
+//
+//void afisareStudent(Student s) {
+//	printf("\nStudentul %s are varsta %d si media %.2f",
+//		s.nume, s.varsta, s.medie);
+//}
+//void push(Nod** varf, Student s) {
+//	Nod* nou = (Nod*)malloc(sizeof(Nod));
+//	nou->info = s;
+//	nou->next = *varf;
+//	*varf = nou;
+//}
+//
+//Student pop(Nod** varf) {
+//	if (*varf == NULL) {
+//		return initStudent(NULL, 0, 0);
+//
+//	}
+//	Student s = (*varf)->info;
+//	Nod* temp = *varf;
+//	*varf = (*varf)->next;
+//	free(temp);
+//
+//	return s;
+//}
+//
+//void put(Nod** coada, Student s) {
+//	Nod* nou = (Nod*)malloc(sizeof(Nod));
+//	nou->info = s;
+//	nou->next = NULL;
+//
+//	if (*coada == NULL) {
+//		*coada = nou;
+//	}
+//	else {
+//		Nod* temp = *coada;
+//		while (temp->next != NULL) {
+//			temp = temp->next;
+//		}
+//		temp->next = nou;
+//	}
+//
+//}
+//
+//int main() {
+//
+//	Nod* stiva = NULL;
+//
+//	push(&stiva, initStudent("Ana", 21, 7.5));
+//	push(&stiva, initStudent("Andreea", 19, 7.5));
+//	push(&stiva, initStudent("Vica", 22, 8));
+//
+//
+//	printf("\nTraversare stiva: ");
+//
+//	while (stiva != NULL) {
+//		Student s = pop(&stiva);
+//		afisareStudent(s);
+//		free(s.nume);
+//	}
+//
+//	Nod* coada = NULL;
+//
+//	put(&coada, initStudent("Ana", 21, 7.5));
+//	put(&coada, initStudent("Andreea", 19, 7.5));
+//	put(&coada, initStudent("Vica", 22, 8));
+//
+//
+//	printf("\nTraversare coada: ");
+//
+//	while (coada != NULL) {
+//		Student s = pop(&coada);
+//		afisareStudent(s);
+//		free(s.nume);
+//	}
+//
+//	return 0;
+//}
