f0cf178d3111955af5d4421d42d94eca7e245cd0
Xonia88 <118845211+Xonia88@users.noreply.github.com>
2026-04-24 11:03:06 +0300

    HashTable Comenzi

---
+++ HashTable_Comenzi.c
@@ -0,0 +1,143 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<malloc.h>
+#include<string.h>
+
+typedef struct Comanda Comanda;
+typedef struct Nod Nod;
+typedef struct HashTable HashTable;
+
+struct Comanda {
+	char* client;
+	int idComanda;
+	float valoare;
+};
+
+struct Nod {
+	Comanda info;
+	Nod* next;
+};
+
+struct HashTable {
+	int dimensiune;
+	Nod** vector;
+};
+
+Comanda initComanda(const char* client, int idComanda, float valoare) {
+	Comanda c;
+	c.idComanda = idComanda;
+	c.valoare = valoare;
+
+	c.client = (char*)malloc(sizeof(char) * (strlen(client) + 1));
+	strcpy(c.client, client);
+
+	return c;
+}
+
+void afisareComanda(Comanda c) {
+	printf("\nComanda clientului %s (id %d) are valoarea %.2f",
+		c.client, c.idComanda, c.valoare);
+}
+
+void afisareLista(Nod* cap) {
+	while (cap != NULL) {
+		afisareComanda(cap->info);
+		cap = cap->next;
+	}
+}
+
+void inserareLaSfarsit(Nod** cap, Comanda c) {
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->info = c;
+	nou->next = NULL;
+
+	if (*cap == NULL) {
+		*cap = nou;
+	}
+	else {
+		Nod* temp = *cap;
+		while (temp->next != NULL) {
+			temp = temp->next;
+		}
+		temp->next = nou;
+	}
+}
+
+HashTable initHashTable(int dim) {
+	HashTable ht;
+	ht.dimensiune = dim;
+	ht.vector = (Nod**)malloc(sizeof(Nod*) * dim);
+
+	for (int i = 0; i < dim; i++) {
+		ht.vector[i] = NULL;
+	}
+
+	return ht;
+}
+
+int hash(int dim, int id) {
+	return id % dim;
+}
+
+void inserareHashTable(HashTable ht, Comanda c) {
+	if (ht.dimensiune > 0) {
+		int poz = hash(ht.dimensiune, c.idComanda);
+		if (poz >= 0 && poz < ht.dimensiune) {
+			inserareLaSfarsit(&(ht.vector[poz]), c);
+		}
+	}
+}
+
+void afisareHashTable(HashTable ht) {
+	for (int i = 0; i < ht.dimensiune; i++) {
+		printf("\nPozitie %d:", i);
+		afisareLista(ht.vector[i]);
+	}
+}
+
+float valoareMaximaPozitia1(HashTable ht) {
+	float max = 0;
+	Nod* cap = ht.vector[1];
+
+	while (cap != NULL) {
+		if (cap->info.valoare > max) {
+			max = cap->info.valoare;
+		}
+		cap = cap->next;
+	}
+
+	return max;
+}
+
+void dezalocareLista(Nod** cap) {
+	while (*cap != NULL) {
+		free((*cap)->info.client);
+		Nod* temp = *cap;
+		*cap = (*cap)->next;
+		free(temp);
+	}
+}
+
+void dezalocareHashTable(HashTable ht) {
+	for (int i = 0; i < ht.dimensiune; i++) {
+		dezalocareLista(&(ht.vector[i]));
+	}
+	free(ht.vector);
+}
+
+int main() {
+	HashTable ht = initHashTable(4);
+
+	inserareHashTable(ht, initComanda("Ana", 10, 250));
+	inserareHashTable(ht, initComanda("Ion", 15, 400));
+	inserareHashTable(ht, initComanda("Maria", 21, 600));
+
+	afisareHashTable(ht);
+
+	printf("\n\nValoarea maxima din pozitia 1: %.2f",
+		valoareMaximaPozitia1(ht));
+
+	dezalocareHashTable(ht);
+
+	return 0;
+}
--- hashtable_produse.c
+++ hashtable_produse.c
@@ -1,139 +1,139 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<malloc.h>
-#include<string.h>
-
-typedef struct Produs Produs;
-typedef struct Nod Nod;
-typedef struct HashTable HashTable;
-
-struct Produs {
-	char* nume;
-	int cod;
-	float pret;
-};
-
-struct Nod {
-	Produs info;
-	Nod* next;
-};
-
-struct HashTable {
-	int dimensiune;
-	Nod** vector;
-};
-
-Produs initProdus(const char* nume, int cod, float pret) {
-	Produs p;
-	p.cod = cod;
-	p.pret = pret;
-	p.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
-	strcpy(p.nume, nume);
-	return p;
-}
-
-void afisareProdus(Produs p) {
-	printf("\nProdusul %s (cod %d) are pretul %.2f",
-		p.nume, p.cod, p.pret);
-}
-
-void afisareLista(Nod* cap) {
-	while (cap != NULL) {
-		afisareProdus(cap->info);
-		cap = cap->next;
-	}
-}
-
-void inserareLaSfarsit(Nod** cap, Produs p) {
-	Nod* nou = (Nod*)malloc(sizeof(Nod));
-	nou->info = p;
-	nou->next = NULL;
-
-	if (*cap == NULL) {
-		*cap = nou;
-	}
-	else {
-		Nod* temp = *cap;
-		while (temp->next != NULL) {
-			temp = temp->next;
-		}
-		temp->next = nou;
-	}
-}
-
-HashTable initHashTable(int dim) {
-	HashTable ht;
-	ht.dimensiune = dim;
-	ht.vector = (Nod**)malloc(sizeof(Nod*) * dim);
-
-	for (int i = 0; i < dim; i++) {
-		ht.vector[i] = NULL;
-	}
-
-	return ht;
-}
-
-int hash(int dim, int cod) {
-	return cod % dim;
-}
-
-void inserareHashTable(HashTable ht, Produs p) {
-	if (ht.dimensiune > 0) {
-		int poz = hash(ht.dimensiune, p.cod);
-		if (poz >= 0 && poz < ht.dimensiune) {
-			inserareLaSfarsit(&(ht.vector[poz]), p);
-		}
-	}
-}
-
-void afisareHashTable(HashTable ht) {
-	for (int i = 0; i < ht.dimensiune; i++) {
-		printf("\nPozitie %d:", i);
-		afisareLista(ht.vector[i]);
-	}
-}
-
-float sumaPreturiPozitia0(HashTable ht) {
-	float suma = 0;
-	Nod* cap = ht.vector[0];
-
-	while (cap != NULL) {
-		suma += cap->info.pret;
-		cap = cap->next;
-	}
-
-	return suma;
-}
-
-void dezalocareLista(Nod** cap) {
-	while (*cap != NULL) {
-		free((*cap)->info.nume);
-		Nod* temp = *cap;
-		*cap = (*cap)->next;
-		free(temp);
-	}
-}
-
-void dezalocareHashTable(HashTable ht) {
-	for (int i = 0; i < ht.dimensiune; i++) {
-		dezalocareLista(&(ht.vector[i]));
-	}
-	free(ht.vector);
-}
-
-int main() {
-	HashTable ht = initHashTable(4);
-
-	inserareHashTable(ht, initProdus("Laptop", 10, 3500));
-	inserareHashTable(ht, initProdus("Telefon", 15, 2000));
-	inserareHashTable(ht, initProdus("Mouse", 22, 150));
-
-	afisareHashTable(ht);
-
-	printf("\n\nSuma preturilor din pozitia 0: %.2f",
-		sumaPreturiPozitia0(ht));
-
-	dezalocareHashTable(ht);
-
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<malloc.h>
+//#include<string.h>
+//
+//typedef struct Produs Produs;
+//typedef struct Nod Nod;
+//typedef struct HashTable HashTable;
+//
+//struct Produs {
+//	char* nume;
+//	int cod;
+//	float pret;
+//};
+//
+//struct Nod {
+//	Produs info;
+//	Nod* next;
+//};
+//
+//struct HashTable {
+//	int dimensiune;
+//	Nod** vector;
+//};
+//
+//Produs initProdus(const char* nume, int cod, float pret) {
+//	Produs p;
+//	p.cod = cod;
+//	p.pret = pret;
+//	p.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
+//	strcpy(p.nume, nume);
+//	return p;
+//}
+//
+//void afisareProdus(Produs p) {
+//	printf("\nProdusul %s (cod %d) are pretul %.2f",
+//		p.nume, p.cod, p.pret);
+//}
+//
+//void afisareLista(Nod* cap) {
+//	while (cap != NULL) {
+//		afisareProdus(cap->info);
+//		cap = cap->next;
+//	}
+//}
+//
+//void inserareLaSfarsit(Nod** cap, Produs p) {
+//	Nod* nou = (Nod*)malloc(sizeof(Nod));
+//	nou->info = p;
+//	nou->next = NULL;
+//
+//	if (*cap == NULL) {
+//		*cap = nou;
+//	}
+//	else {
+//		Nod* temp = *cap;
+//		while (temp->next != NULL) {
+//			temp = temp->next;
+//		}
+//		temp->next = nou;
+//	}
+//}
+//
+//HashTable initHashTable(int dim) {
+//	HashTable ht;
+//	ht.dimensiune = dim;
+//	ht.vector = (Nod**)malloc(sizeof(Nod*) * dim);
+//
+//	for (int i = 0; i < dim; i++) {
+//		ht.vector[i] = NULL;
+//	}
+//
+//	return ht;
+//}
+//
+//int hash(int dim, int cod) {
+//	return cod % dim;
+//}
+//
+//void inserareHashTable(HashTable ht, Produs p) {
+//	if (ht.dimensiune > 0) {
+//		int poz = hash(ht.dimensiune, p.cod);
+//		if (poz >= 0 && poz < ht.dimensiune) {
+//			inserareLaSfarsit(&(ht.vector[poz]), p);
+//		}
+//	}
+//}
+//
+//void afisareHashTable(HashTable ht) {
+//	for (int i = 0; i < ht.dimensiune; i++) {
+//		printf("\nPozitie %d:", i);
+//		afisareLista(ht.vector[i]);
+//	}
+//}
+//
+//float sumaPreturiPozitia0(HashTable ht) {
+//	float suma = 0;
+//	Nod* cap = ht.vector[0];
+//
+//	while (cap != NULL) {
+//		suma += cap->info.pret;
+//		cap = cap->next;
+//	}
+//
+//	return suma;
+//}
+//
+//void dezalocareLista(Nod** cap) {
+//	while (*cap != NULL) {
+//		free((*cap)->info.nume);
+//		Nod* temp = *cap;
+//		*cap = (*cap)->next;
+//		free(temp);
+//	}
+//}
+//
+//void dezalocareHashTable(HashTable ht) {
+//	for (int i = 0; i < ht.dimensiune; i++) {
+//		dezalocareLista(&(ht.vector[i]));
+//	}
+//	free(ht.vector);
+//}
+//
+//int main() {
+//	HashTable ht = initHashTable(4);
+//
+//	inserareHashTable(ht, initProdus("Laptop", 10, 3500));
+//	inserareHashTable(ht, initProdus("Telefon", 15, 2000));
+//	inserareHashTable(ht, initProdus("Mouse", 22, 150));
+//
+//	afisareHashTable(ht);
+//
+//	printf("\n\nSuma preturilor din pozitia 0: %.2f",
+//		sumaPreturiPozitia0(ht));
+//
+//	dezalocareHashTable(ht);
+//
+//	return 0;
+//}
