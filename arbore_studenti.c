a788eb8b2a4ceba736b48762a5ea64ee056be3ca
Xonia88 <118845211+Xonia88@users.noreply.github.com>
2026-05-06 21:25:30 +0300

    arbore - studenti

---
+++ arbore_studenti.c
@@ -0,0 +1,204 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct StructuraStudent {
+	int id;
+	int varsta;
+	float medie;
+	char* nume;
+	char* facultate;
+	unsigned char grupa;
+};
+typedef struct StructuraStudent Student;
+
+struct Nod {
+	Student info;
+	struct Nod* st;
+	struct Nod* dr;
+};
+typedef struct Nod Nod;
+
+Student citireStudentDinFisier(FILE* file) {
+	char buffer[100];
+	char sep[3] = ",\n";
+	char* aux;
+
+	Student s;
+
+	if (fgets(buffer, 100, file) == NULL) {
+		s.id = -1;
+		return s;
+	}
+
+	aux = strtok(buffer, sep);
+	s.id = atoi(aux);
+	s.varsta = atoi(strtok(NULL, sep));
+	s.medie = (float)atof(strtok(NULL, sep));
+
+	aux = strtok(NULL, sep);
+	s.nume = (char*)malloc(strlen(aux) + 1);
+	strcpy(s.nume, aux);
+
+	aux = strtok(NULL, sep);
+	s.facultate = (char*)malloc(strlen(aux) + 1);
+	strcpy(s.facultate, aux);
+
+	s.grupa = *strtok(NULL, sep);
+
+	return s;
+}
+
+void afisareStudent(Student s) {
+	printf("Id: %d\n", s.id);
+	printf("Varsta: %d\n", s.varsta);
+	printf("Medie: %.2f\n", s.medie);
+	printf("Nume: %s\n", s.nume);
+	printf("Facultate: %s\n", s.facultate);
+	printf("Grupa: %c\n\n", s.grupa);
+}
+
+void adaugaStudentInArbore(Nod** rad, Student sNou) {
+	if (*rad == NULL) {
+		Nod* nod = (Nod*)malloc(sizeof(Nod));
+		nod->info = sNou;
+		nod->st = NULL;
+		nod->dr = NULL;
+		*rad = nod;
+	}
+	else {
+		if ((*rad)->info.id > sNou.id) {
+			adaugaStudentInArbore(&((*rad)->st), sNou);
+		}
+		if ((*rad)->info.id < sNou.id) {
+			adaugaStudentInArbore(&((*rad)->dr), sNou);
+		}
+	}
+}
+
+Nod* citireArboreDinFisier(const char* numeFisier) {
+	Nod* rad = NULL;
+	FILE* f = fopen(numeFisier, "r");
+
+	if (f) {
+		while (1) {
+			Student s = citireStudentDinFisier(f);
+			if (s.id == -1) break;
+			adaugaStudentInArbore(&rad, s);
+		}
+	}
+	fclose(f);
+	return rad;
+}
+
+void afisareInordine(Nod* rad) {
+	if (rad) {
+		afisareInordine(rad->st);
+		afisareStudent(rad->info);
+		afisareInordine(rad->dr);
+	}
+}
+
+void afisarePreordine(Nod* rad) {
+	if (rad) {
+		afisareStudent(rad->info);
+		afisarePreordine(rad->st);
+		afisarePreordine(rad->dr);
+	}
+}
+
+void dezalocareArbore(Nod** rad) {
+	if (*rad) {
+		dezalocareArbore(&(*rad)->st);
+		dezalocareArbore(&(*rad)->dr);
+		free((*rad)->info.nume);
+		free((*rad)->info.facultate);
+		free(*rad);
+		*rad = NULL;
+	}
+}
+
+Student getStudentByID(Nod* rad, int id) {
+	Student s;
+	s.id = -1;
+
+	if (rad) {
+		if (rad->info.id == id) {
+			s = rad->info;
+			s.nume = (char*)malloc(strlen(rad->info.nume) + 1);
+			strcpy(s.nume, rad->info.nume);
+			s.facultate = (char*)malloc(strlen(rad->info.facultate) + 1);
+			strcpy(s.facultate, rad->info.facultate);
+		}
+		if (id < rad->info.id) {
+			s = getStudentByID(rad->st, id);
+		}
+		if (id > rad->info.id) {
+			s = getStudentByID(rad->dr, id);
+		}
+	}
+	return s;
+}
+
+int determinaNumarNoduri(Nod* rad) {
+	if (rad) {
+		return determinaNumarNoduri(rad->st) +
+			determinaNumarNoduri(rad->dr) + 1;
+	}
+	return 0;
+}
+
+int maxim(int a, int b) {
+	return (a > b ? a : b);
+}
+
+int calculeazaInaltime(Nod* rad) {
+	if (rad) {
+		return maxim(calculeazaInaltime(rad->st),
+			calculeazaInaltime(rad->dr)) + 1;
+	}
+	return 0;
+}
+
+float calculeazaMedieTotala(Nod* rad) {
+	if (rad) {
+		return rad->info.medie +
+			calculeazaMedieTotala(rad->st) +
+			calculeazaMedieTotala(rad->dr);
+	}
+	return 0;
+}
+
+float calculeazaMediiFacultate(Nod* rad, const char* facultate) {
+	if (rad) {
+		float suma = calculeazaMediiFacultate(rad->st, facultate) +
+			calculeazaMediiFacultate(rad->dr, facultate);
+
+		if (strcmp(rad->info.facultate, facultate) == 0) {
+			suma += rad->info.medie;
+		}
+		return suma;
+	}
+	return 0;
+}
+
+int main() {
+	Nod* rad = citireArboreDinFisier("studenti.txt");
+
+	printf("Preordine:\n");
+	afisarePreordine(rad);
+
+	printf("Student cautat:\n");
+	afisareStudent(getStudentByID(rad, 2));
+
+	printf("Nr noduri: %d\n", determinaNumarNoduri(rad));
+	printf("Inaltime: %d\n", calculeazaInaltime(rad));
+	printf("Suma medii: %.2f\n", calculeazaMedieTotala(rad));
+	printf("Suma medii ASE: %.2f\n",
+		calculeazaMediiFacultate(rad, "ASE"));
+
+	dezalocareArbore(&rad);
+
+	return 0;
+}
--- heap_studenti.c
+++ heap_studenti.c
@@ -1,184 +1,184 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct Student Student;
-typedef struct Heap Heap;
-
-struct Student {
-	int id;
-	int varsta;
-	float medie;
-	char* nume;
-	char* facultate;
-	unsigned char grupa;
-};
-
-struct Heap {
-	int lungime;
-	Student* studenti;
-	int nrElemente;
-};
-
-Student citireStudentDinFisier(FILE* file) {
-	char buffer[100];
-	char sep[3] = ",\n";
-	Student s;
-	s.id = -1;
-	s.nume = NULL;
-	s.facultate = NULL;
-
-	if (fgets(buffer, 100, file) == NULL) {
-		return s;
-	}
-
-	char* aux;
-
-	aux = strtok(buffer, sep);
-	if (aux != NULL) s.id = atoi(aux);
-
-	aux = strtok(NULL, sep);
-	if (aux != NULL) s.varsta = atoi(aux);
-
-	aux = strtok(NULL, sep);
-	if (aux != NULL) s.medie = (float)atof(aux);
-
-	aux = strtok(NULL, sep);
-	if (aux != NULL) {
-		s.nume = (char*)malloc(strlen(aux) + 1);
-		strcpy(s.nume, aux);
-	}
-
-	aux = strtok(NULL, sep);
-	if (aux != NULL) {
-		s.facultate = (char*)malloc(strlen(aux) + 1);
-		strcpy(s.facultate, aux);
-	}
-
-	aux = strtok(NULL, sep);
-	if (aux != NULL) {
-		s.grupa = aux[0];
-	}
-
-	return s;
-}
-
-void afisareStudent(Student s) {
-	printf("\nId: %d", s.id);
-	printf("\nVarsta: %d", s.varsta);
-	printf("\nMedie: %.2f", s.medie);
-	printf("\nNume: %s", s.nume ? s.nume : "-");
-	printf("\nFacultate: %s", s.facultate ? s.facultate : "-");
-	printf("\nGrupa: %c\n", s.grupa);
-}
-
-Heap initializareHeap(int lungime) {
-	Heap heap;
-	heap.lungime = lungime;
-	heap.studenti = (Student*)malloc(sizeof(Student) * lungime);
-	heap.nrElemente = 0;
-	return heap;
-}
-
-void filtreazaHeap(Heap heap, int pozitieNod) {
-	int st = 2 * pozitieNod + 1;
-	int dr = 2 * pozitieNod + 2;
-	int min = pozitieNod;
-
-	if (st < heap.nrElemente && heap.studenti[min].medie > heap.studenti[st].medie) {
-		min = st;
-	}
-
-	if (dr < heap.nrElemente && heap.studenti[min].medie > heap.studenti[dr].medie) {
-		min = dr;
-	}
-
-	if (min != pozitieNod) {
-		Student aux = heap.studenti[min];
-		heap.studenti[min] = heap.studenti[pozitieNod];
-		heap.studenti[pozitieNod] = aux;
-
-		if (min < (heap.nrElemente - 2) / 2) {
-			filtreazaHeap(heap, min);
-		}
-	}
-}
-
-Heap citireHeapDinFisier(const char* numeFisier) {
-	Heap heap = initializareHeap(10);
-	FILE* f = fopen(numeFisier, "r");
-
-	if (f == NULL) {
-		printf("Fisierul nu a fost gasit!\n");
-		return heap;
-	}
-
-	while (1) {
-		Student s = citireStudentDinFisier(f);
-		if (s.id == -1) break;
-
-		heap.studenti[heap.nrElemente++] = s;
-	}
-
-	fclose(f);
-
-	for (int i = (heap.nrElemente - 2) / 2; i >= 0; i--) {
-		filtreazaHeap(heap, i);
-	}
-
-	return heap;
-}
-
-void afisareHeap(Heap heap) {
-	for (int i = 0; i < heap.nrElemente; i++) {
-		afisareStudent(heap.studenti[i]);
-	}
-}
-
-Student extrageStudent(Heap* heap) {
-	Student s;
-	s.id = -1;
-
-	if (heap->nrElemente > 0) {
-		s = heap->studenti[0];
-		heap->studenti[0] = heap->studenti[heap->nrElemente - 1];
-		heap->nrElemente--;
-
-		for (int i = (heap->nrElemente - 2) / 2; i >= 0; i--) {
-			filtreazaHeap(*heap, i);
-		}
-	}
-
-	return s;
-}
-
-void dezalocareHeap(Heap* heap) {
-	for (int i = 0; i < heap->nrElemente; i++) {
-		if (heap->studenti[i].nume != NULL)
-			free(heap->studenti[i].nume);
-
-		if (heap->studenti[i].facultate != NULL)
-			free(heap->studenti[i].facultate);
-	}
-	free(heap->studenti);
-}
-
-int main() {
-
-	Heap heap = citireHeapDinFisier("studenti.txt");
-
-	printf("\nHeap:");
-	afisareHeap(heap);
-
-	printf("\nExtrageri:");
-	afisareStudent(extrageStudent(&heap));
-	afisareStudent(extrageStudent(&heap));
-
-	printf("\nHeap ramas:");
-	afisareHeap(heap);
-
-	dezalocareHeap(&heap);
-
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//typedef struct Student Student;
+//typedef struct Heap Heap;
+//
+//struct Student {
+//	int id;
+//	int varsta;
+//	float medie;
+//	char* nume;
+//	char* facultate;
+//	unsigned char grupa;
+//};
+//
+//struct Heap {
+//	int lungime;
+//	Student* studenti;
+//	int nrElemente;
+//};
+//
+//Student citireStudentDinFisier(FILE* file) {
+//	char buffer[100];
+//	char sep[3] = ",\n";
+//	Student s;
+//	s.id = -1;
+//	s.nume = NULL;
+//	s.facultate = NULL;
+//
+//	if (fgets(buffer, 100, file) == NULL) {
+//		return s;
+//	}
+//
+//	char* aux;
+//
+//	aux = strtok(buffer, sep);
+//	if (aux != NULL) s.id = atoi(aux);
+//
+//	aux = strtok(NULL, sep);
+//	if (aux != NULL) s.varsta = atoi(aux);
+//
+//	aux = strtok(NULL, sep);
+//	if (aux != NULL) s.medie = (float)atof(aux);
+//
+//	aux = strtok(NULL, sep);
+//	if (aux != NULL) {
+//		s.nume = (char*)malloc(strlen(aux) + 1);
+//		strcpy(s.nume, aux);
+//	}
+//
+//	aux = strtok(NULL, sep);
+//	if (aux != NULL) {
+//		s.facultate = (char*)malloc(strlen(aux) + 1);
+//		strcpy(s.facultate, aux);
+//	}
+//
+//	aux = strtok(NULL, sep);
+//	if (aux != NULL) {
+//		s.grupa = aux[0];
+//	}
+//
+//	return s;
+//}
+//
+//void afisareStudent(Student s) {
+//	printf("\nId: %d", s.id);
+//	printf("\nVarsta: %d", s.varsta);
+//	printf("\nMedie: %.2f", s.medie);
+//	printf("\nNume: %s", s.nume ? s.nume : "-");
+//	printf("\nFacultate: %s", s.facultate ? s.facultate : "-");
+//	printf("\nGrupa: %c\n", s.grupa);
+//}
+//
+//Heap initializareHeap(int lungime) {
+//	Heap heap;
+//	heap.lungime = lungime;
+//	heap.studenti = (Student*)malloc(sizeof(Student) * lungime);
+//	heap.nrElemente = 0;
+//	return heap;
+//}
+//
+//void filtreazaHeap(Heap heap, int pozitieNod) {
+//	int st = 2 * pozitieNod + 1;
+//	int dr = 2 * pozitieNod + 2;
+//	int min = pozitieNod;
+//
+//	if (st < heap.nrElemente && heap.studenti[min].medie > heap.studenti[st].medie) {
+//		min = st;
+//	}
+//
+//	if (dr < heap.nrElemente && heap.studenti[min].medie > heap.studenti[dr].medie) {
+//		min = dr;
+//	}
+//
+//	if (min != pozitieNod) {
+//		Student aux = heap.studenti[min];
+//		heap.studenti[min] = heap.studenti[pozitieNod];
+//		heap.studenti[pozitieNod] = aux;
+//
+//		if (min < (heap.nrElemente - 2) / 2) {
+//			filtreazaHeap(heap, min);
+//		}
+//	}
+//}
+//
+//Heap citireHeapDinFisier(const char* numeFisier) {
+//	Heap heap = initializareHeap(10);
+//	FILE* f = fopen(numeFisier, "r");
+//
+//	if (f == NULL) {
+//		printf("Fisierul nu a fost gasit!\n");
+//		return heap;
+//	}
+//
+//	while (1) {
+//		Student s = citireStudentDinFisier(f);
+//		if (s.id == -1) break;
+//
+//		heap.studenti[heap.nrElemente++] = s;
+//	}
+//
+//	fclose(f);
+//
+//	for (int i = (heap.nrElemente - 2) / 2; i >= 0; i--) {
+//		filtreazaHeap(heap, i);
+//	}
+//
+//	return heap;
+//}
+//
+//void afisareHeap(Heap heap) {
+//	for (int i = 0; i < heap.nrElemente; i++) {
+//		afisareStudent(heap.studenti[i]);
+//	}
+//}
+//
+//Student extrageStudent(Heap* heap) {
+//	Student s;
+//	s.id = -1;
+//
+//	if (heap->nrElemente > 0) {
+//		s = heap->studenti[0];
+//		heap->studenti[0] = heap->studenti[heap->nrElemente - 1];
+//		heap->nrElemente--;
+//
+//		for (int i = (heap->nrElemente - 2) / 2; i >= 0; i--) {
+//			filtreazaHeap(*heap, i);
+//		}
+//	}
+//
+//	return s;
+//}
+//
+//void dezalocareHeap(Heap* heap) {
+//	for (int i = 0; i < heap->nrElemente; i++) {
+//		if (heap->studenti[i].nume != NULL)
+//			free(heap->studenti[i].nume);
+//
+//		if (heap->studenti[i].facultate != NULL)
+//			free(heap->studenti[i].facultate);
+//	}
+//	free(heap->studenti);
+//}
+//
+//int main() {
+//
+//	Heap heap = citireHeapDinFisier("studenti.txt");
+//
+//	printf("\nHeap:");
+//	afisareHeap(heap);
+//
+//	printf("\nExtrageri:");
+//	afisareStudent(extrageStudent(&heap));
+//	afisareStudent(extrageStudent(&heap));
+//
+//	printf("\nHeap ramas:");
+//	afisareHeap(heap);
+//
+//	dezalocareHeap(&heap);
+//
+//	return 0;
+//}
