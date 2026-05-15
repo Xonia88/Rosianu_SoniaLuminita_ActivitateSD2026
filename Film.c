4bf91e6923e96f63ab4d0f3f4b8a788f46874d67
Xonia88 <118845211+Xonia88@users.noreply.github.com>
2026-05-15 20:38:24 +0300

    Film - arbore binar(inserare, afisare)

---
+++ Film.c
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct Film {
+
+	int id;
+	char* titlu;
+
+}Film;
+
+typedef struct Nod {
+
+	Film info;
+
+	struct Nod* st;
+	struct Nod* dr;
+
+}Nod;
+
+Film creareFilm(int id, const char* titlu) {
+
+	Film f;
+
+	f.id = id;
+
+	f.titlu = (char*)malloc(strlen(titlu) + 1);
+
+	strcpy(f.titlu, titlu);
+
+	return f;
+}
+
+void inserare(Nod** rad, Film f) {
+
+	if (*rad == NULL) {
+
+		Nod* nou = (Nod*)malloc(sizeof(Nod));
+
+		nou->info = f;
+
+		nou->st = NULL;
+		nou->dr = NULL;
+
+		*rad = nou;
+	}
+	else {
+
+		if (f.id < (*rad)->info.id) {
+
+			inserare(&((*rad)->st), f);
+		}
+		else {
+
+			inserare(&((*rad)->dr), f);
+		}
+	}
+}
+
+void afisare(Nod* rad) {
+
+	if (rad != NULL) {
+
+		afisare(rad->st);
+
+		printf("\n%d %s",
+			rad->info.id,
+			rad->info.titlu);
+
+		afisare(rad->dr);
+	}
+}
+
+int main() {
+
+	Nod* rad = NULL;
+
+	inserare(&rad, creareFilm(5, "Titanic"));
+	inserare(&rad, creareFilm(2, "Avatar"));
+	inserare(&rad, creareFilm(8, "Joker"));
+
+	afisare(rad);
+
+	return 0;
+}
--- exersarepentrulucrare.c
+++ exersarepentrulucrare.c
@@ -1,53 +1,53 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
-struct Test {
-	char* materie;
-	int nrStudenti;
-	float medie;
-};
-
-typedef struct Test Test;
-
-struct Nod {
-	Test info;
-	struct Nod* next;
-
-};
-
-typedef struct Nod Nod;
-
-Test initTest(const char* materie, int nrStudenti, float medie) {
-
-	Test t;
-
-	t.nrStudenti = nrStudenti;
-	t.medie = medie;
-	t.materie = (char*)malloc(strlen(materie) + 1);
-	strcpy(t.materie, materie);
-
-	return t;
-
-}
-
-void afisareTest(Test t) {
-
-	printf("\n%s, %d, %.2f",
-		t.materie,
-		t.nrStudenti,
-		t.medie);
-}
-
-int main() {
-
-	Test t = initTest("SIE", 50, 7.9);
-
-	afisareTest(t);
-
-	free(t.materie);
-
-	return 0;
-
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//
+//struct Test {
+//	char* materie;
+//	int nrStudenti;
+//	float medie;
+//};
+//
+//typedef struct Test Test;
+//
+//struct Nod {
+//	Test info;
+//	struct Nod* next;
+//
+//};
+//
+//typedef struct Nod Nod;
+//
+//Test initTest(const char* materie, int nrStudenti, float medie) {
+//
+//	Test t;
+//
+//	t.nrStudenti = nrStudenti;
+//	t.medie = medie;
+//	t.materie = (char*)malloc(strlen(materie) + 1);
+//	strcpy(t.materie, materie);
+//
+//	return t;
+//
+//}
+//
+//void afisareTest(Test t) {
+//
+//	printf("\n%s, %d, %.2f",
+//		t.materie,
+//		t.nrStudenti,
+//		t.medie);
+//}
+//
+//int main() {
+//
+//	Test t = initTest("SIE", 50, 7.9);
+//
+//	afisareTest(t);
+//
+//	free(t.materie);
+//
+//	return 0;
+//
+//}
